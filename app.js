e8ea382c9c4c9d40c23fa744e9f108a45b41ba17
Matias Gonzalez <mng_epson@hotmail.com>
2022-08-29 11:02:49 -0300

    feat: agregando elementos a la lista

--- app.js
+++ app.js
@@ -18,10 +18,34 @@
 
 // }
 
-let itemList = document.querySelector('#items');
+// let itemList = document.querySelector('#items');
 
-console.log(itemList.firstElementChild);
+// console.log(itemList.firstElementChild);
 
-itemList.firstElementChild.style.fontSize = ('30px');
+// itemList.firstElementChild.style.fontSize = ('30px');
 
-itemList.lastElementChild.style.color = ('blue');
+// itemList.lastElementChild.style.color = ('blue');
+
+//Agregar elementos a la lista
+
+let form = document.getElementById('formAgregar');
+let lista = document.getElementById('items');
+
+form.addEventListener('submit', agregarItem);
+
+function agregarItem(e){
+    e.preventDefault();
+    let nuevoItem = document.getElementById('item').value;
+
+    let li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.appendChild(document.createTextNode(nuevoItem));
+
+    let boton = document.createElement('button');
+    boton.className = 'btn btn-danger btn-sm float-right';
+    boton.appendChild(document.createTextNode('x'));
+
+    li.appendChild(boton);
+
+    lista.appendChild(li);
+}
