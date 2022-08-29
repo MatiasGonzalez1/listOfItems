83f784ae0adca85dc186c8cd057ca1e289a48fea
Matias Gonzalez <mng_epson@hotmail.com>
2022-08-29 12:33:35 -0300

    feat: add delete function

--- app.js
+++ app.js
@@ -31,8 +31,13 @@
 let form = document.getElementById('formAgregar');
 let lista = document.getElementById('items');
 
+//evento submit del formulario
 form.addEventListener('submit', agregarItem);
 
+//evento click de la lista
+lista.addEventListener('click', eliminarItem);
+
+//funcion para agregar un item a la lista
 function agregarItem(e){
     e.preventDefault();
     let nuevoItem = document.getElementById('item').value;
@@ -42,10 +47,20 @@ function agregarItem(e){
     li.appendChild(document.createTextNode(nuevoItem));
 
     let boton = document.createElement('button');
-    boton.className = 'btn btn-danger btn-sm float-right';
+    boton.className = 'btn btn-danger btn-sm float-right eliminar';
     boton.appendChild(document.createTextNode('x'));
 
     li.appendChild(boton);
 
     lista.appendChild(li);
+}
+
+//funcion para eliminar un item a la lista
+function eliminarItem(e){
+if(e.target.classList.contains('eliminar')){
+    if(confirm('¿Seguro que desea eliminar este elemento?')){
+        let li = e.target.parentElement;
+    lista.removeChild(li);
+    }
+}
 }
